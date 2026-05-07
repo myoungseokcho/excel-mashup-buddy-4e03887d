20bd531a363c057d295b939e53def8ed958f3a4e
myoungseokcho <myoungseokcho@gmail.com>
2026-05-07 23:34:06 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/excel-mashup-buddy-4e03887d">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
